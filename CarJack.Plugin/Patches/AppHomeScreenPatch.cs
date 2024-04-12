21882fbee0b178bc344cfdf998628d35fef45893
Nahuel Rocchetti <Nahuel3d@gmail.com>
2024-04-12 16:26:20 -0300

    make camera not available in cars

---
+++ CarJack.Plugin/Patches/AppHomeScreenPatch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarJack.Common;
+using HarmonyLib;
+using Reptile;
+using Reptile.Phone;
+
+namespace CarJack.Plugin.Patches
+{
+    [HarmonyPatch(typeof(AppHomeScreen))]
+    internal static class AppHomeScreenPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch(nameof(AppHomeScreen.OnPressRight))]
+        private static bool OnPressRight_Prefix(AppHomeScreen __instance)
+        {
+            if (CarController.Instance == null) return true;
+            var inVehicle = CarController.Instance.CurrentCar != null;
+            if (!inVehicle) return true;
+            var appToOpen = __instance.m_ScrollView.SelectedButtton as HomescreenButton;
+            if (appToOpen.AssignedApp.appType != Reptile.HomeScreenApp.HomeScreenAppType.CAMERA) return true;
+            __instance.m_AudioManager.PlaySfxGameplay(SfxCollectionID.PhoneSfx, AudioClipID.FlipPhone_Back);
+            return false;
+        }
+    }
+}
